8bd8ca2032e22576e5af8efaade8d358818e285e
awasiak-visa <awasiak@visa.com>
2025-09-06 23:44:39 +0200

    Added CRUD operations for Category and Publisher

--- src/main/java/pl/coderslab/boardgame/BoardGameService.java
+++ src/main/java/pl/coderslab/boardgame/BoardGameService.java
@@ -2,6 +2,8 @@ package pl.coderslab.boardgame;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class BoardGameService {
     private final BoardGameRepository boardGameRepository;
@@ -13,5 +15,34 @@ public class BoardGameService {
         this.boardGameRepository = boardGameRepository;
         this.categoryRepository = categoryRepository;
         this.publisherRepository = publisherRepository;
+    }
+
+
+    // Category
+    public void createCategory(Category category) {
+        categoryRepository.save(category);
+    }
+    public Optional<Category> readCategory(Long id) {
+        return categoryRepository.findById(id);
+    }
+    public void updateCategory(Category category) {
+        categoryRepository.update(category.getName(), category.getId());
+    }
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    // Publisher
+    public void createPublisher(Publisher publisher) {
+        publisherRepository.save(publisher);
+    }
+    public Optional<Publisher> readPublisher(Long id) {
+        return publisherRepository.findById(id);
+    }
+    public void updatePublisher(Publisher publisher) {
+        publisherRepository.update(publisher.getName(), publisher.getId());
+    }
+    public void deletePublisher(Long id) {
+        publisherRepository.deleteById(id);
     }
 }
--- src/main/java/pl/coderslab/boardgame/CategoryRepository.java
+++ src/main/java/pl/coderslab/boardgame/CategoryRepository.java
@@ -1,8 +1,16 @@
 package pl.coderslab.boardgame;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("update Category c set c.name=?1 where c.id=?2")
+    void update(String name, Long id);
 }
